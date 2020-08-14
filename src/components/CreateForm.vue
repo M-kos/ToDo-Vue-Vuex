670537fd167f35d1c4d4207d4afd5a5568452b07
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-15 01:00:06 +0300

    Форма создания

---
+++ src/components/CreateForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="create-form-overlay">
+    <div class="create-form">
+      <div class="create-form-title">create {{name}}</div>
+      <div class="create-form-body">
+        <input type="text" class="create-form-input" placeholder="Title" v-model="titleValue">
+        <textarea class="create-form-description" placeholder="Description" v-model="descriptionValue" v-if="name === 'card'"></textarea>
+      </div>
+      <div class="create-form-action">
+        <button
+          class="btn"
+          :class="{disabled: !titleValue}"
+          @click.prevent="onCreate"
+        >
+          Create
+        </button>
+        <button class="btn" @click.prevent="onClose">Close</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateForm",
+
+  props: {
+    name: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data() {
+    return {
+      titleValue: '',
+      descriptionValue: ''
+    }
+  },
+
+  methods: {
+    onCreate() {
+      this.$emit('create', {
+        title: this.titleValue,
+        description: this.descriptionValue,
+        id: Date.now(),
+        done: false
+      })
+    },
+
+    onClose() {
+      this.$emit('close', false)
+    }
+  } 
+};
+</script>
