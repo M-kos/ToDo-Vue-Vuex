28435f8808105af78cf1fb46b031e93bc2f4606c
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-14 18:39:56 +0300

    Добавил фильтры

--- src/components/GroupCards.vue
+++ src/components/GroupCards.vue
@@ -2,8 +2,22 @@
   <div class="group group-cards">
     <div class="group-header">
       <h2 class="group-title">{{title}}</h2>
-      <button class="btn group-btn-delete">
+      <button class="btn group-btn-delete" @click="onDeleteGroup">
         <i class="material-icons group-icon-delete" v-if="custom">delete</i>
+      </button>
+    </div>
+    <div class="group-filters">
+      <input class="group-filters-input" type="text" placeholder="Search..." v-model="searchValue">
+      <div class="flex-fill"></div>
+      <button
+        v-for="button in filterButtons"
+        :key="button.name"
+        class="btn"
+        :class="{active: button.name === activeFilter}"
+        :name="button.name"
+        @click="onClickFilterButton"
+      >
+        {{button.label}}
       </button>
     </div>
     <CardItem
@@ -13,7 +27,7 @@
       :description="card.description"
       :done="card.done"
     />
-    <button class="btn group-btn-add">
+    <button class="btn group-btn-add" @click="onAddCard">
       <i class="large material-icons group-icon-add">add</i>
     </button>
   </div>
@@ -40,6 +54,39 @@ export default {
       type: Boolean,
       default: false
     }
+  },
+
+  data() {
+    return {
+      filterButtons: [
+        {
+          name: 'all',
+          label: 'All'
+        },
+        {
+          name: 'active',
+          label: 'Active'
+        },
+        {
+          name: 'done',
+          label: 'Done'
+        }
+      ],
+
+      activeFilter: 'all',
+
+      searchValue: ''
+    }
+  },
+
+  methods: {
+    onClickFilterButton(event) {
+      this.activeFilter = event.target.name
+    },
+
+    onAddCard() {},
+
+    onDeleteGroup() {}
   },
 
   components: {
