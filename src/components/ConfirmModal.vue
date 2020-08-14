e9cce7ddab81783fa129405f7e876629d722da08
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-15 00:59:43 +0300

    Модальное окно с подтверждением

---
+++ src/components/ConfirmModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="confirm-modal-overlay">
+    <div class="confirm-modal">
+      <div class="confirm-modal-title">are you sure?</div>
+      <div class="confirm-modal-action">
+        <button class="btn" @click.prevent="onConfirm(true)">Ok</button>
+        <button class="btn" @click.prevent="onConfirm(false)">Close</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConfirmModal",
+
+  methods: {
+    onConfirm(value) {
+      this.$emit('confirm', value)
+    }
+  } 
+};
+</script>
