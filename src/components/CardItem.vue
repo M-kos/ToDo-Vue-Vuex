e4a99efa3e772850cc3785a242e78c032079ae07
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-15 05:12:20 +0300

    Перетаскивание карточек Обрезание текста

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="card-item" draggable="true">
+  <div
+    class="card-item"
+    :class="{'drag-start': isDrag}"
+    draggable="true"
+    @dragstart="dragHandler"
+    @dragend="dragHandler"
+  >
     <div class="card-item-header">
       <label
         class="check"
@@ -9,13 +15,32 @@
         <input class="check-input" type="checkbox">
         <i class="material-icons check-icon" v-if="card.done">done</i>
       </label>
-      <h3 class="card-item-title" :class="{ 'line-through': card.done }">{{card.title}}</h3> 
+
+      <h3 class="card-item-title text-overflow" :class="{ 'line-through': card.done }">
+        {{card.title}}
+      </h3> 
+
       <div class="flex-fill"></div>
+
       <button class="btn card-item-btn-delete" @click.prevent="toggleShowModal">
         <i class="material-icons">delete</i>
       </button>
     </div>
-    <div class="card-item-desctiption" :class="{ 'line-through': card.done }">{{card.description}}</div>
+
+    <div class="card-item-desctiption">
+      <span ref="textCotainer" :class="{ 'line-through': card.done }">
+        {{card.description}}
+      </span>
+
+      <div class="flex-fill"></div>
+
+      <button class="btn" @click="handlerTextContainer" v-if="isLongString">
+        <i class="material-icons">
+          {{isExpand ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}}
+        </i>
+      </button>
+    </div>
+
     <ConfirmModal
       v-if="showConfirmModal"
       @confirm="onConfirm"
@@ -39,7 +64,10 @@ export default {
 
   data() {
     return {
-      showConfirmModal: false
+      showConfirmModal: false,
+      isDrag: false,
+      isExpand: false,
+      isLongString: false
     }
   },
 
@@ -59,10 +87,29 @@ export default {
       this.toggleShowModal()
     },
 
+    dragHandler(event) {
+      event.dataTransfer.setData('text/plain', this.card.id)
+      this.isDrag = !this.isDrag
+    },
+
+    handlerTextContainer() {
+      this.isExpand = !this.isExpand
+
+      this.$refs.textCotainer.classList.toggle('expand')
+    },
+
     ...mapActions([
       'changeCardStatus',
       'deleteItem'
     ])
+  },
+
+  mounted() {
+    if (this.$refs && this.$refs.textCotainer && this.$refs.textCotainer.offsetWidth > 600) {
+      this.$refs.textCotainer.style.display = 'block'
+      this.$refs.textCotainer.style.width = '90%'
+      this.isLongString = true
+    }
   },
 
   components: {
--- src/components/GroupCards.vue
+++ src/components/GroupCards.vue
@@ -1,42 +1,66 @@
 <template>
-  <div class="group group-cards">
-    <div class="group-header">
-      <h2 class="group-title">{{group.title}}</h2>
-      <button class="btn group-btn-delete" @click="toggleShow('modal')">
-        <i class="material-icons" v-if="group.custom">delete</i>
+  <div>
+    <div
+      class="group"
+      @dragenter.stop.prevent="toggleDrag(true)"
+      @dragover.prevent=""
+    >
+      <div
+        class="group-drop-overlay"
+        v-if="isDragEnter"
+        @dragleave="toggleDrag(false)"
+        @drop.prevent="onDrop"
+      ></div>
+
+      <div class="group-header">
+        <h2 class="group-title text-overflow">{{group.title}}</h2>
+
+        <button class="btn group-btn-delete" @click="toggleShow('modal')">
+          <i class="material-icons" v-if="group.custom">delete</i>
+        </button>
+      </div>
+
+      <div class="group-filters">
+        <input class="group-filters-input" type="text" placeholder="Search..." v-model="searchValue">
+
+        <div class="flex-fill"></div>
+
+        <div class="group-filters-buttons">
+          <button
+            v-for="button in filterButtons"
+            :key="button.name"
+            class="btn"
+            :class="{active: button.name === activeFilter}"
+            :name="button.name"
+            @click="onClickFilterButton"
+          >
+            {{button.label}}
+          </button>
+        </div>
+
+      </div>
+
+      <CardItem
+        v-for="card in displayedCardsWithFilter"
+        :key="card.id"
+        :card="card"
+      />
+
+      <button class="btn group-btn-add" @click="toggleShow('form')">
+        <i class="large material-icons group-icon-add">add</i>
       </button>
+
+      <ConfirmModal
+        v-if="show.modal"
+        @confirm="onConfirm"
+      />
     </div>
-    <div class="group-filters">
-      <input class="group-filters-input" type="text" placeholder="Search..." v-model="searchValue">
-      <div class="flex-fill"></div>
-      <button
-        v-for="button in filterButtons"
-        :key="button.name"
-        class="btn"
-        :class="{active: button.name === activeFilter}"
-        :name="button.name"
-        @click="onClickFilterButton"
-      >
-        {{button.label}}
-      </button>
-    </div>
-    <CardItem
-      v-for="card in displayedCards"
-      :key="card.id"
-      :card="card"
-    />
-    <button class="btn group-btn-add" @click="toggleShow('form')">
-      <i class="large material-icons group-icon-add">add</i>
-    </button>
+
     <CreateForm
       name="card"
       v-if="show.form"
       @close="toggleShow('form')"
       @create="createCard"
-    />
-    <ConfirmModal
-      v-if="show.modal"
-      @confirm="onConfirm"
     />
   </div>
 </template>
@@ -79,6 +103,8 @@ export default {
         modal: false
       },
 
+      isDragEnter: false,
+
       activeFilter: 'all',
       searchValue: ''
     }
@@ -93,6 +119,18 @@ export default {
             card.title.toLowerCase().includes(this.searchValue.toLowerCase())
           )
         })
+    },
+
+    displayedCardsWithFilter() {
+      switch(this.activeFilter) {
+        case 'active':
+          return this.displayedCards.filter(card => !card.done)
+        case 'done':
+          return this.displayedCards.filter(card => card.done)
+        case 'all':
+        default:
+           return this.displayedCards
+      }
     },
 
     ...mapGetters([
@@ -129,9 +167,23 @@ export default {
       this.toggleShow('modal')
     },
 
+    toggleDrag(value) {
+      this.isDragEnter = value
+    },
+
+    onDrop(event) {
+      const cardId = +event.dataTransfer.getData("text")
+      this.changeItemGroup({
+        id: cardId,
+        groupId: this.group.id
+      })
+      this.toggleDrag(false)
+    },
+
     ...mapActions([
       'addItem',
-      'deleteItem'
+      'deleteItem',
+      'changeItemGroup'
     ])
   },
 
